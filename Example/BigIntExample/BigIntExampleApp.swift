0f387536c5ad5f388d3fcccf727d370e31fee34b
Andrew Zheng <aheze@getfind.app>
2024-04-02 02:07:04 -0700

    Add codable conformance

---
+++ Example/BigIntExample/BigIntExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  BigIntExampleApp.swift
+//  BigIntExample
+//  
+//  Created by Andrew Zheng (github.com/aheze) on 4/2/24.
+//  Copyright © 2024 Andrew Zheng. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct BigIntExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Example/BigIntExample/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  BigIntExample
+//  
+//  Created by Andrew Zheng (github.com/aheze) on 4/2/24.
+//  Copyright © 2024 Andrew Zheng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
